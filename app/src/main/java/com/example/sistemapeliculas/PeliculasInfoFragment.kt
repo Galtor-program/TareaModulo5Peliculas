5ad04242dc419336036f53245fdacf2c3f8a0929
Felipe Toro Galdámez - galtor <felipetorog@gmail.com>
2024-05-14 10:42:36 -0400

    Commit Final

--- app/src/main/java/com/example/sistemapeliculas/PeliculasInfoFragment.kt
+++ app/src/main/java/com/example/sistemapeliculas/PeliculasInfoFragment.kt
@@ -1,8 +1,11 @@
 package com.example.sistemapeliculas
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.sistemapeliculas.adapters.PeliculaAdapter
 import com.example.sistemapeliculas.databinding.PeliculaInfoFragmentBinding
 import com.example.sistemapeliculas.entities.Pelicula
@@ -21,7 +24,7 @@ class PeliculasInfoFragment: BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.pelicula_info_fragment,container,false)
+        return inflater.inflate(R.layout.pelicula_info_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,8 +34,25 @@ class PeliculasInfoFragment: BottomSheetDialogFragment() {
         binding.imagenPelicula.setImageResource(currentPelicula.imageResId)
         binding.directorTxt.text = currentPelicula.Director
         binding.anoPeliculatxt.text = currentPelicula.añoCreacion.toString()
-
+        binding.shareTxt.setOnClickListener {
+            enviarCorreoPelicula(currentPelicula)
+        }
 
     }
 
+    private fun enviarCorreoPelicula(pelicula: Pelicula) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("felipetorog@gmail.com"))
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Envío de Pelicula")
+        intent.putExtra(
+            Intent.EXTRA_TEXT, "Envio de datos de pelicula"
+                    + pelicula.nombre + ",Director: " + pelicula.Director + ", Fecha: " + pelicula.añoCreacion
+        )
+        startActivity(Intent.createChooser(intent,"Enviar Correo"))
+
+    }
 }
+
+
+
